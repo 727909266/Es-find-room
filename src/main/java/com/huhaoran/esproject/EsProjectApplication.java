56f82ae1e00641fc1afe40fa714d01b1d8f576aa
huhaoran1 <huhaoran1@xiaomi.com>
2019-01-29 11:59:16 +0800

    init

---
+++ src/main/java/com/huhaoran/esproject/EsProjectApplication.java
@@ -0,0 +1,14 @@
+package com.huhaoran.esproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsProjectApplication.class, args);
+    }
+
+}
+
